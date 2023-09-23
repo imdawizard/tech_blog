5695dbefc8818af6d9e65fce313556e8c3b7c082
Spyrodragonman Mannyman <nate.is.cool.yeah@gmail.com>
2023-09-23 11:32:27 -0600

    added a commentroutes.js file for comment routes

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// Route to create a new comment
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      content: req.body.content,
+      user_id: req.session.user_id, // The ID of the user who created the comment
+      blogpost_id: req.body.blogpost_id, // The ID of the associated blog post
+    });
+
+    res.status(200).json(newComment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
