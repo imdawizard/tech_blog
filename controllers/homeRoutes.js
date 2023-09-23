73cd0941f63fbcbdd0578305d873e14103fd9e11
Spyrodragonman Mannyman <nate.is.cool.yeah@gmail.com>
2023-09-23 12:16:22 -0600

    fixed blogpostdetail.handlebars file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -46,14 +46,16 @@ router.get('/blogposts/:id', async (req, res) => {
     const postId = req.params.id;
     // Fetch the blog post data for the specified ID
     const blogpost = await Blogpost.findByPk(postId, {
-      include: [{ model: Comment, include: User }], // Include comments and their creators
+      // include: [{ model: Comment, include: User }], // Include comments and their creators
     });
     if (!blogpost) {
       // Handle the case where the blog post is not found
       return res.status(404).render('error'); // You can create an error handle view
     }
+    const blogpostData = blogpost.toJSON();
     // Render the "blogPostDetail" view and pass the blogpost data to it
     res.render('blogPostDetail', { blogpost });
+    console.log(blogpost);
   } catch (err) {
     res.status(500).json(err);
   }
